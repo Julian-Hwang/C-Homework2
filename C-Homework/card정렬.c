aaa4ecb1a01a19038375016383a15b007a3c3a27
julian5383 <julian5383@kku.ac.kr>
2022-03-15 00:55:20 +0900

    third commit

--- C-Homework/card정렬.c
+++ C-Homework/card정렬.c
@@ -26,5 +26,74 @@ int main()
 	print_deck(deck);
 	printf("\n");
 
+	sort(deck);
+	print_deck(deck);
+	printf("\n");
 
+	return 0;
+}
+
+void init_deck(CARD arr[])
+{
+	int i, face, suit;
+	
+	i = 0;
+	for ( suit = 0; suit < 4; suit++)
+	{
+		for ( face = 1; face <= 13; face++)
+		{
+			arr[i].suit = suit;
+			arr[i].face = face;
+			i++;
+		}
+	}
+}
+
+void shuffle_deck(CARD arr[])
+{
+	int first, second;
+	CARD s;
+
+	srand(time(NULL));
+	for ( first = 0; first < 52; first++)
+	{
+		second = rand() % 52;
+		s = arr[first];
+		arr[first] = arr[second];
+		arr[second] = s;
+	}
+}
+
+void print_card(CARD* c)
+{
+	char* faces[] = { "","A","2","3","4","5","6","7","8","9","10","J","Q","K" };
+	char* suits[] = { "Hearts","Diamond","Clubs","Spades" };
+	printf("%s-%s \t", faces[c->face], suits[c->suit]);
+}
+
+void print_deck(CARD arr[])
+{
+	int i;
+	for ( i = 0; i < 52; i++)
+	{
+		print_card(&arr[i]);
+		if (i % 4 == 3) {
+			printf("\n");
+		}
+	}
+}
+
+void sort(CARD arr[])
+{
+	int i, j, index = 0;
+
+	for ( i = 1; i <= 13; i++)
+	{
+		for ( j = 0; j < 4; j++)
+		{
+			arr[index].face = i;
+			arr[index].suit = j;
+			index++;
+		}
+	}
 }
